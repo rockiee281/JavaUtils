84530a14c92242f03426cd25842bde01de6e50a1
liyun <rockiee281@gmail.com>
2013-07-08 23:07:57 +0800

    增加对多音字的支持，目前还未完成，需要增加后向纠错

--- src/com/liyun/contacts/MyContactsMatch.java
+++ src/com/liyun/contacts/MyContactsMatch.java
@@ -9,13 +9,15 @@ public class MyContactsMatch {
 	private String base;
 
 	private String target;
-	private String[] baseWords;
+	private String[][] baseWords;
 	private int[] matchWordPositions;
 	private Integer[] wordType; // 联系人字符类型,1为中文，否则为非中文
 	private char[] targetChar;
-	private char[][] baseCharArrayPerWord;
+	private char[][][] baseCharArrayPerWord; // 保存联系人按照char拆分之后的信息，中文多音字最多匹配
 	private final static int WORDTYPE_CHINESE = 1;
 	private final static int WORDTYPE_NO_CHINESE = 0;
+	private int currentMatchCursor = -1; // 保存当前匹配到的单词游标
+	private int tIndex = 0; // 记录用户输入词目前的匹配位置
 
 	public MyContactsMatch(String base, String target) {
 		this.base = base;
@@ -25,15 +27,17 @@ public class MyContactsMatch {
 
 	private void init() {
 		// 将中文或中英文混杂的联系人信息转化为拼音
-		List<String> wordList = new ArrayList<String>(20);
+		List<String[]> wordList = new ArrayList<String[]>(20);
 		List<Integer> wordTypeList = new ArrayList<Integer>(20);
 		StringBuilder notChineseChar = new StringBuilder();
 		char[] tmpCharArray = base.toCharArray();
-		for (int i = 0; i < tmpCharArray.length;i++) {
+		for (int i = 0; i < tmpCharArray.length; i++) {
 			if (PinyinUtil.isChinese(tmpCharArray[i])) {
-				String pinyin = PinyinUtil.toContactPinyin(tmpCharArray[i]);
+				String[] pinyin = PinyinUtil.toContactPinyin(tmpCharArray[i]);
 				if (notChineseChar.length() > 0) {
-					wordList.add(notChineseChar.toString());
+					String[] notChinese = new String[1];
+					notChinese[0] = notChineseChar.toString();
+					wordList.add(notChinese);
 					notChineseChar.delete(0, notChineseChar.length());
 					wordTypeList.add(WORDTYPE_NO_CHINESE); // 非中文
 				}
@@ -45,21 +49,26 @@ public class MyContactsMatch {
 		}
 
 		if (notChineseChar.length() > 0) { // 最后以字母等结尾
-			wordList.add(notChineseChar.toString());
+			String[] notChinese = new String[1];
+			notChinese[0] = notChineseChar.toString();
+			wordList.add(notChinese);
 			notChineseChar.delete(0, notChineseChar.length());
 		}
 
 		// 得到处理后的word数组以及word类型数组
-		this.baseWords = wordList.toArray(new String[wordList.size()]);
+		this.baseWords = wordList.toArray(new String[wordList.size()][]);
 		this.wordType = wordTypeList.toArray(new Integer[wordTypeList.size()]);
 
 		// 初始化匹配数组
 		matchWordPositions = new int[base.length()];
 
 		targetChar = target.toCharArray();
-		baseCharArrayPerWord = new char[baseWords.length][];
+		baseCharArrayPerWord = new char[baseWords.length][][];
 		for (int i = 0; i < baseWords.length; i++) {
-			baseCharArrayPerWord[i] = baseWords[i].toCharArray();
+			baseCharArrayPerWord[i] = new char[baseWords[i].length][];
+			for (int j = 0; j < baseWords[i].length; j++) {
+				baseCharArrayPerWord[i][j] = baseWords[i][j].toCharArray();
+			}
 		}
 
 	}
@@ -92,65 +101,89 @@ public class MyContactsMatch {
 
 	private boolean checkSub(int startBaseWordIndex) {
 
-		int tIndex = 0; // 记录用户输入词目前的匹配位置
-		int currentMatchCursor = -1; // 保存当前匹配到的单词游标
+		tIndex = 0; // 记录用户输入词目前的匹配位置
 		for (int wordIndex = startBaseWordIndex; wordIndex < baseWords.length; wordIndex++) {
-			char[] baseCharOfWord = baseCharArrayPerWord[wordIndex];
+			char[][] baseCharOfWord = baseCharArrayPerWord[wordIndex];
 			int myWordType = wordType[wordIndex];
 
 			// 记录初始游标以及本词长度
 			// 中文长度为1个字符，非中文为字符本身长度
 			int initCursor = currentMatchCursor;
 			int wordLength = 1;
+
+			boolean checkResult = false;
 			if (myWordType == WORDTYPE_NO_CHINESE) {
 				// 本词为非中文时的初始化工作
 				wordLength = baseCharOfWord.length;
+				checkResult = checkWord(baseCharOfWord[0], WORDTYPE_NO_CHINESE, wordIndex);
 			} else {
 				// 本词为中文时的初始化工作
-				currentMatchCursor++;
+				currentMatchCursor++; // 游标位置退后一位
+				int initTIndex = tIndex; // 记录初始的tIndex，方便匹配失败的时候回到初始值
+				for (int multiIndex = 0; multiIndex < baseCharOfWord.length; multiIndex++) {
+					char[] charOfWord = baseCharOfWord[multiIndex];
+					if (checkWord(charOfWord, WORDTYPE_CHINESE, wordIndex)) {
+						checkResult = true;
+						// 匹配成功
+						break;
+					} else {
+						// 匹配失败
+						tIndex = initTIndex;
+					}
+				}
 			}
 
-			for (int charIndex = 0; charIndex < baseCharOfWord.length; charIndex++) {
-				if (wordLength == WORDTYPE_NO_CHINESE) {
-					currentMatchCursor++;
-				}
-
-				if (baseCharOfWord[charIndex] == targetChar[tIndex]) {
-					tIndex++;
-					matchWordPositions[currentMatchCursor] = 1;
-					if (tIndex == targetChar.length) { // match all the target
-														// char
-						return true;
-					}
-				} else { // 当前不匹配的情况
-
-					if (startBaseWordIndex == baseWords.length - 1) {
-						// 已经到最后一个词，并且未能匹配，则肯定不能匹配
-						return false;
-					} else if (matchWordPositions[wordIndex] > 0) { // 当前词已经匹配过，则跳到下个word
-						break;
-					} else if (tIndex > 0 && targetChar[tIndex - 1] == baseCharArrayPerWord[startBaseWordIndex + 1][0]) {
-						/*
-						 * 当前词未匹配，但是后向纠错可以匹配 比如 “zhang hao
-						 * feng”在输入“zhf”时，zh会首先匹配到zhang，当f匹配到hao时发生错误
-						 * 此时需要将f前面的h做后向纠错以正确的匹配
-						 * 此种情况适用于h会出现在zh\ch\sh特殊声母，以及n、g这种会同时出现在声母及韵母的字母
-						 */
-
-						if (tIndex > 0 && targetChar[tIndex - 1] == baseCharArrayPerWord[wordIndex][0]) {
-							matchWordPositions[wordIndex] = 1;
-							break;
-						}
-					} else {
-						// 本次匹配失败
-						return false;
-					}
-				}
+			if (!checkResult) {
+				return false;
 			}
 
 			currentMatchCursor = initCursor + wordLength;
 		}
 		return false;
+	}
+
+	private boolean checkWord(char[] baseCharOfWord, int wordType, int wordIndex) {
+		boolean matchThisWord = false;
+
+		for (int charIndex = 0; charIndex < baseCharOfWord.length; charIndex++) {
+			if (wordType == WORDTYPE_NO_CHINESE) {
+				currentMatchCursor++;
+			}
+
+			if (baseCharOfWord[charIndex] == targetChar[tIndex]) {
+				tIndex++;
+				matchThisWord = true;
+				matchWordPositions[currentMatchCursor] = 1;
+				if (tIndex == targetChar.length) { // match all the target
+													// char
+					return true;
+				}
+			} else { // 当前不匹配的情况
+
+				if (wordIndex == baseWords.length - 1) {
+					// 已经到最后一个词，并且未能匹配，则肯定不能匹配
+					return false;
+				} else if (matchThisWord) { // 当前词已经匹配过，则跳到下个word
+					return true;
+				} else if (tIndex > 0 && targetChar[tIndex - 1] == baseCharArrayPerWord[startBaseWordIndex + 1][0]) {
+					/*
+					 * 当前词未匹配，但是后向纠错可以匹配 比如 “zhang hao
+					 * feng”在输入“zhf”时，zh会首先匹配到zhang，当f匹配到hao时发生错误
+					 * 此时需要将f前面的h做后向纠错以正确的匹配
+					 * 此种情况适用于h会出现在zh\ch\sh特殊声母，以及n、g这种会同时出现在声母及韵母的字母
+					 */
+
+					if (tIndex > 0 && targetChar[tIndex - 1] == baseCharArrayPerWord[wordIndex][0]) {
+						matchWordPositions[wordIndex] = 1;
+						break;
+					}
+				} else {
+					// 本次匹配失败
+					return false;
+				}
+			}
+		}
+
 	}
 
 	public static void main(String[] args) {
@@ -165,21 +198,5 @@ public class MyContactsMatch {
 		m = new MyContactsMatch("zhang chang zhi", "zhgz");
 		System.out.println(m.check());
 		m.printMatch();
-
-		System.out.println(lcs("zhangchangzhi", "hcz"));
-	}
-
-	public static String lcs(String a, String b) {
-		int aLen = a.length();
-		int bLen = b.length();
-		if (aLen == 0 || bLen == 0) {
-			return "";
-		} else if (a.charAt(aLen - 1) == b.charAt(bLen - 1)) {
-			return lcs(a.substring(0, aLen - 1), b.substring(0, bLen - 1)) + a.charAt(aLen - 1);
-		} else {
-			String x = lcs(a, b.substring(0, bLen - 1));
-			String y = lcs(a.substring(0, aLen - 1), b);
-			return (x.length() > y.length()) ? x : y;
-		}
 	}
 }
--- src/com/liyun/util/PinyinUtil.java
+++ src/com/liyun/util/PinyinUtil.java
@@ -19,15 +19,17 @@ public class PinyinUtil {
 	 * @param chinese
 	 * @return
 	 * 
-	 * TODO 针对中国人的姓名特定发音做优化，设计一个map做映射
+	 *         TODO 针对中国人的姓名特定发音做优化，设计一个map做映射
 	 */
-	public static String toContactPinyin(char chinese) {
+	public static String[] toContactPinyin(char chinese) {
 		if (!isChinese(chinese)) {
-			return String.valueOf(chinese);
+			String[] result =  new String[1];
+			result[0] = String.valueOf(chinese);
+			return result;
 		} else {
 			try {
 				String[] pinyinArray = PinyinHelper.toHanyuPinyinStringArray(chinese, PINYIN_FORMAT);
-				return pinyinArray[0];
+				return pinyinArray;
 			} catch (Exception e) {
 				e.printStackTrace();
 				return null;
@@ -43,12 +45,9 @@ public class PinyinUtil {
 	 */
 	public static final boolean isChinese(char c) {
 		Character.UnicodeBlock ub = Character.UnicodeBlock.of(c);
-		if (ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS
-				|| ub == Character.UnicodeBlock.CJK_COMPATIBILITY_IDEOGRAPHS
-				|| ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_A
-				|| ub == Character.UnicodeBlock.GENERAL_PUNCTUATION
-				|| ub == Character.UnicodeBlock.CJK_SYMBOLS_AND_PUNCTUATION
-				|| ub == Character.UnicodeBlock.HALFWIDTH_AND_FULLWIDTH_FORMS) {
+		if (ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS || ub == Character.UnicodeBlock.CJK_COMPATIBILITY_IDEOGRAPHS
+				|| ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_A || ub == Character.UnicodeBlock.GENERAL_PUNCTUATION
+				|| ub == Character.UnicodeBlock.CJK_SYMBOLS_AND_PUNCTUATION || ub == Character.UnicodeBlock.HALFWIDTH_AND_FULLWIDTH_FORMS) {
 			return true;
 		}
 		return false;
