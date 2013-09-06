62be2def01ac04da72506b6784603ac44dda79e8
liyun <rockiee281@gmail.com>
2013-09-06 18:35:52 +0800

    home link data

--- src/com/liyun/homelinkSpider/HomeSpider.java
+++ src/com/liyun/homelinkSpider/HomeSpider.java
@@ -39,54 +39,41 @@ public class HomeSpider {
 	private final String START_PAGE_URL = "http://beijing.homelink.com.cn/ershoufang/";
 	public final static int UPPER_COST = 215; // 能承受的最高总价
 	private final Log logger = LogFactory.getLog(getClass());
-	private ExecutorService service;
+	private ExecutorService processService;
+	private ExecutorService crawlerService;
 	public final static String HOST = "http://beijing.homelink.com.cn/";
 	private final ConcurrentLinkedQueue<String> queue = new ConcurrentLinkedQueue<String>();
+	private final int crawlerThreadCount = 3;
+	private final int processThreadCount = 1;
+	private final int startPage = 5530; // 抓取起始页
 
 	public HomeSpider() {
-		service = Executors.newFixedThreadPool(5); // 5个线程抓取数据
+		crawlerService = Executors.newFixedThreadPool(crawlerThreadCount); // 爬虫线程池
+		processService = Executors.newFixedThreadPool(processThreadCount); // 线程处理数据
 	}
 
 	public void crawler() {
-		HttpClient httpClient = new DefaultHttpClient();
-		String content = getPageContent(httpClient, START_PAGE_URL);
-
-		// 启动监听线程
+		// 启动文件写入线程
 		WriteFileThread writeFile = new WriteFileThread();
 		Thread thread = new Thread(writeFile);
 		thread.setName("file-writer-thread");
 		thread.start();
 
-		boolean hasNextPage = true;
-
-		do {
-			// 解析页面
-			Document doc = Jsoup.parse(content);
-			// 处理二手房信息列表
-			processList(doc);
-
-			// 判断是否还有更多数据
-			Element nextPageSpan = doc.select("div.fanye").first().select("span.nextpage").first();
-			hasNextPage = nextPageSpan == null ? false : true; // 还有下一页，表面当前并非最后一页
-
-			if (hasNextPage) {
-				String link = nextPageSpan.parent().attr("href");
-				if (StringUtils.isEmpty(link)) {
-					logger.info("获取下一页连接失败");
-					break;
-				} else if (link.startsWith("/")) {
-					link = HOST + link;
-				}
-				content = getPageContent(httpClient, link);
-			}
-
-		} while (hasNextPage);
+		for (int i = 0; i < crawlerThreadCount; i++) {
+			InnerSpider spider = new InnerSpider(startPage + i, crawlerThreadCount, i);
+			crawlerService.execute(spider);
+		}
 
 		try {
-			service.shutdown();	// 主动关闭线程池，会在执行所有任务之后关闭
-			if (service.awaitTermination(1l, TimeUnit.HOURS)) { // 超时时间
-				// do something
-				writeFile.stop();
+			crawlerService.shutdown(); // 发出关闭抓取线程池信号
+			if (crawlerService.awaitTermination(1l, TimeUnit.HOURS)) { // 等待所有抓取线程结束运行
+				logger.info("抓取线程运行结束");
+				processService.shutdown(); // 关闭分析线程池，会在执行所有任务之后关闭
+				if (processService.awaitTermination(1l, TimeUnit.HOURS)) { // 等待数据处理结束
+					logger.info("分析线程运行结束");
+					// 停止文件写入线程
+					writeFile.stop();
+				}
 			}
 		} catch (Exception e) {
 			logger.error("任务超时", e);
@@ -95,6 +82,7 @@ public class HomeSpider {
 
 	private String getPageContent(HttpClient httpClient, String url) {
 		HttpGet get = new HttpGet(url);
+		setCommonRequestHeader(get);
 		ResponseHandler<String> rspHandler = new BasicResponseHandler();
 		try {
 			String content = httpClient.execute(get, rspHandler);
@@ -116,7 +104,7 @@ public class HomeSpider {
 		for (Element element : houseList) {
 			try {
 				HomeLinkeDetailPageTask task = new HomeLinkeDetailPageTask(element, queue);
-				service.execute(task);
+				processService.execute(task);
 			} catch (Exception e) {
 				logger.error("处理数据失败，" + element, e);
 			}
@@ -126,6 +114,18 @@ public class HomeSpider {
 	public static void main(String[] args) {
 		HomeSpider spider = new HomeSpider();
 		spider.crawler();
+	}
+
+	/**
+	 * 设置通用header
+	 * */
+	private void setCommonRequestHeader(HttpGet method) {
+		method.addHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+		method.addHeader("Connection", "keep-alive");
+		method.addHeader("Cache-Control", "max-age=0");
+		method.addHeader("Referer", "http://beijing.homelink.com.cn/ershoufang/");
+		method.addHeader("User-Agent",
+				"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.62 Safari/537.36");
 	}
 
 	/**
@@ -170,6 +170,7 @@ public class HomeSpider {
 
 			if (out != null) {
 				try {
+					out.flush();
 					out.close();
 				} catch (IOException e) {
 					e.printStackTrace();
@@ -180,5 +181,54 @@ public class HomeSpider {
 		public void stop() {
 			this.hasMore = false;
 		}
+	}
+
+	/**
+	 * 内部类，用于爬取内容
+	 * */
+	class InnerSpider extends Thread {
+		private int pageNum;
+		private int mod;
+
+		public InnerSpider(int startPage, int mod, int index) {
+			setName("crawler-thread-" + index);
+			this.pageNum = startPage;
+			this.mod = mod;
+		}
+
+		@Override
+		public void run() {
+			HttpClient httpClient = new DefaultHttpClient();
+			String link = START_PAGE_URL + "pg" + pageNum + "/";
+			String content = getPageContent(httpClient, link);
+			boolean hasNextPage = true;
+			do {
+				// 解析页面
+				Document doc = Jsoup.parse(content);
+				// 处理二手房信息列表
+				processList(doc);
+
+				// 判断是否还有更多数据
+				Element nextPageSpan = doc.select("div.fanye").first().select("span.nextpage").first();
+				hasNextPage = nextPageSpan == null ? false : true; // 还有下一页，表面当前并非最后一页
+
+				if (hasNextPage) {
+					pageNum += mod;
+					link = START_PAGE_URL + "pg" + pageNum + "/";
+					content = getPageContent(httpClient, link);
+				} else {
+					logger.info(link);
+					logger.info("抓取结束，么有更多数据了，当前是[" + pageNum + "]页");
+				}
+				try {
+					Thread.sleep(1000 * 5); // 抓太快容易出问题，每次休息5s好了
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+
+			} while (hasNextPage);
+
+		}
+
 	}
 }
